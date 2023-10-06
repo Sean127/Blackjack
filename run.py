148d5fd66bd2acb20a78eaef15b194c80f761d3f
Sean127 <sdoran401@gmail.com>
2023-10-06 21:16:14 +0000

    Add card deck and player and dealer hand creation

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+deck = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']*4
+
+
+def deal_player_hands(deck):
+    ''' Creates Players hand '''
+    player_hand = []
+
+    for i in range(2):
+        random.shuffle(deck)
+        card = deck.pop()
+        player_hand.append(card)
+    return player_hand
+
+
+def deal_dealer_hand(deck):
+    ''' Creates Dealers hand '''
+    dealer_hand = []
+
+    for i in range(2):
+        random.shuffle(deck)
+        card = deck.pop()
+        dealer_hand.append(card)
+    return dealer_hand
+
+
